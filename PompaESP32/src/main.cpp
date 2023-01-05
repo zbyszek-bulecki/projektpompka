1f489db9a566231f2d1c76d3db5675b1e5793ce1
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-01-05 20:50:35 +0100

    added reading ssid & pwd from config file on sdcard

--- PompaESP32/src/main.cpp
+++ PompaESP32/src/main.cpp
@@ -5,10 +5,10 @@
 #include "SharkFrameManager.h"
 #include "FS.h"
 #include "SD.h"
+#include "SPI.h"
 #include "FileOperations.h"
 
-const char *SSID = "Pancernik";
-const char *PASSWORD = "KawkaKawusia@2k21";
+const char *WIFI_CONFIG_PATH = "/config/wifi.txt";
 
 Sensors sensors(34, 2700, 1000, 0x4A, 0x76, 0x20, 7);
 uint32_t interval = 600000;
@@ -21,15 +21,15 @@ void sendSensorsValue()
   Serial.print("Connecting to ");
   Serial.println(host);
 
-  // WiFiClient client;
-  // if (!client.connect(host, port))
-  // {
-  //   Serial.println("Connection failed.");
-  //   Serial.println("Waiting 5 seconds before retrying...");
-  //   delay(5000);
-  //   return;
-  // }
-  // digitalWrite(LED_BUILTIN, HIGH);
+  WiFiClient client;
+  if (!client.connect(host, port))
+  {
+    Serial.println("Connection failed.");
+    Serial.println("Waiting 5 seconds before retrying...");
+    delay(5000);
+    return;
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
 
   DynamicJsonDocument doc(1024);
   doc["address"] = "0"; // WiFi.macAddress();
@@ -49,11 +49,11 @@ void sendSensorsValue()
   voltage = voltage * 2.1;
   doc["voltage"] = String(voltage, 2);
 
-  // SharkFrameManager manager = SharkFrameManager(client);
+  SharkFrameManager manager = SharkFrameManager(client);
   String outJson;
   serializeJson(doc, outJson);
   Serial.println(outJson);
-  // manager.send("sensors", outJson);
+  manager.send("sensors", outJson);
 
   String data;
   serializeJson(doc, data);
@@ -71,54 +71,78 @@ void sendSensorsValue()
 
   readFile(SD, "/logs/sensorData.txt");
 
-//  SharkMessage msg = manager.read();
+  SharkMessage msg = manager.read();
 
-// if (msg.success)
-// {
-//   Serial.print("key: ");
-//   Serial.println(msg.key.c_str());
-//   Serial.print("value: ");
-//   Serial.println(msg.value.c_str());
-// }
+  if (msg.success)
+  {
+    Serial.print("key: ");
+    Serial.println(msg.key.c_str());
+    Serial.print("value: ");
+    Serial.println(msg.value.c_str());
+  }
 
-// Serial.println("Closing connection.");
-// client.stop();
-// digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Closing connection.");
+  client.stop();
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void setup()
 {
+
+  if (!SD.begin())
+  {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+
+  std::string ssid;
+  std::string wifiPassword;
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(32, INPUT);
   sensors.begin();
 
-  if(!SD.begin()){
-        Serial.println("Card Mount Failed");
-        return;
+  if (!SD.exists(WIFI_CONFIG_PATH))
+  {
+    char wifiConfig[] = "ssid=password";
+    createDir(SD, "/config");
+    writeFile(SD, WIFI_CONFIG_PATH, wifiConfig);
+    Serial.println("Blank WiFi config data created.");
+  }
+  else
+  {
+
+    File myfile = SD.open(WIFI_CONFIG_PATH);
+    std::string wifiConfig;
+    if (myfile)
+    {
+      while (myfile.available())
+      {
+        wifiConfig += (myfile.read());
+      }
+      myfile.close();
     }
-    uint8_t cardType = SD.cardType();
-
-    if(cardType == CARD_NONE){
-        Serial.println("No SD card attached");
-        return;
+    else
+    {
+      Serial.println("Error opening WiFi config file.");
     }
+    Serial.println(wifiConfig.c_str());
 
-    Serial.print("SD Card Type: ");
-    if(cardType == CARD_MMC){
-        Serial.println("MMC");
-    } else if(cardType == CARD_SD){
-        Serial.println("SDSC");
-    } else if(cardType == CARD_SDHC){
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
+    char *p;
+    p = strchr(wifiConfig.c_str(), '=');
+    wifiPassword.append(p + 1);
+    Serial.println(wifiPassword.c_str());
+
+    int b;
+    b = (wifiConfig.length() - wifiPassword.length() - 1);
+    for (int i = 0; i < b; i++)
+    {
+      ssid += wifiConfig[i];
     }
+    Serial.println(ssid.c_str());
+  }
 
-    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-    Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-  WiFi.begin(SSID, PASSWORD);
+  WiFi.begin(ssid.c_str(), wifiPassword.c_str());
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
