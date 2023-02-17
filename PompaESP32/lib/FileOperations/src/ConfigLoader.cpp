7d652128d2f5627baa57e4f608773418b4135e7c
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-02-17 23:26:34 +0100

    modifying ConfigLoader to work with struct

--- PompaESP32/lib/FileOperations/src/ConfigLoader.cpp
+++ PompaESP32/lib/FileOperations/src/ConfigLoader.cpp
@@ -2,11 +2,22 @@
 #include "SD.h"
 #include "SPI.h"
 
-const char *WIFI_CONFIG_PATH = "/config/wifi.txt";
+const char *WIFI_CONFIG_PATH = "/config/config.txt";
+
+    struct esp32config
+  {
+    String ssid;
+    String wifPwd;
+    uint16_t server_ip_address;
+    uint16_t server_port;
+    uint16_t sleepTime;
+  };
+
+esp32config.ssid = "kokoszka";
+
 
 void loadConfiguration()
 {
-    
       if (!SD.begin())
   {
     Serial.println("Card Mount Failed");
@@ -14,8 +25,6 @@ void loadConfiguration()
   }
     File myfile = SD.open(WIFI_CONFIG_PATH);
     std::string wifiConfig;
-    std::string wifiPassword;
-    std::string ssid;
 
     if (myfile)
     {
@@ -34,11 +43,12 @@ void loadConfiguration()
 
     char *p;
     p = strchr(wifiConfig.c_str(), '=');
-    wifiPassword.append(p + 1);
-    Serial.println(wifiPassword.c_str());
+    esp32config.
+    wifiPwd.append(p + 1);
+    Serial.println(wifiPwd.c_str());
 
     int b;
-    b = (wifiConfig.length() - wifiPassword.length() - 1);
+    b = (wifiConfig.length() - wifiPwd.length() - 1);
     
     for (int i = 0; i < b; i++)
     {
@@ -47,13 +57,3 @@ void loadConfiguration()
 
     Serial.println(ssid.c_str());
 }
-
-// struct esp32config
-//   {
-//     String ssid;
-//     String wifPwd;
-//     String server_name;
-//     uint16_t server_ip_address;
-//     uint16_t server_port;
-//     uint16_t sleepTime;
-//   };
