d90e89bbda21c76178ce076daf08dad06803d201
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-02-25 22:55:01 +0100

    working on parsing the config :construction_worker:

--- PompaESP32/lib/FileOperations/src/ConfigLoader.cpp
+++ PompaESP32/lib/FileOperations/src/ConfigLoader.cpp
@@ -44,6 +44,10 @@ char *loadConfiguration(const char *configFilePath)
             for (size_t i = 0; i < 500; i++)
             {
                 config[i] = file.read();
+                if (config[i] == '\0')
+                {
+                    file.close();
+                }
             }
         }
         file.close();
@@ -58,19 +62,39 @@ char *loadConfiguration(const char *configFilePath)
 void parseConfiguration(const char *configFilePath)
 {
     loadConfiguration(configFilePath);
-    // int numberOfLines = lineCounter(configFilePath);
-    // char *configLines[numberOfLines];
-    // char endOfLine = '\n';
+    int numberOfLines = lineCounter(configFilePath);
+    String *configLines[numberOfLines];
+    String *configKeys[numberOfLines];
+    String *configValues[numberOfLines];
+    int j = 0;
 
-    Serial.print(config);
+    // Serial.println(config);
+    // Serial.println(numberOfLines);
 
-    // for (size_t i = 0; i < numberOfLines; i++)
-    // {
-    //     Serial.print(". ");
-    //     // TODO: Parsing single config lines and writing them to struct.
-    //     // TODO: Implement fixed buffer max size 128 bytes or 2x 64 bytes buffer, if more text than 128 bytes, abort reading the line.
-    //     // TODO: Check for correct encoding of text file (UTF-8).
-    //     // TODO: Check if the characters are from the correct ASCII range.
-    //     // TODO: Check if the number of characters in a line does not exceed a reasonable limit.
-    // }
+    for (size_t i = 0; i < 500; i++)
+    {
+        while (config[i] != '=')
+        {
+            configKeys[j]->concat(config[i]);
+        }
+        while (config[i] != '\n')
+        {
+            configValues[j]->concat(config[i]);
+        }
+        if (config[i] == '\n')
+        {
+            j++;
+        }
+    }
+    for (size_t i = 0; i < numberOfLines; i++)
+    {
+        Serial.println(configKeys[i]->c_str());
+        Serial.println(configValues[i]->c_str());
+    }
+
+    // TODO: Parsing single config lines and writing them to struct.
+    // TODO: Implement fixed buffer max size 128 bytes or 2x 64 bytes buffer, if more text than 128 bytes, abort reading the line.
+    // TODO: Check for correct encoding of text file (UTF-8).
+    // TODO: Check if the characters are from the correct ASCII range.
+    // TODO: Check if the number of characters in a line does not exceed a reasonable limit.
 }
