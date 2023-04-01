e78c0f7c4093ef80d0c699d556dbcd2c68b50bc5
Filip Bąkowski <94741392+FilipBakowski@users.noreply.github.com>
2023-04-01 11:16:04 +0200

    Delete GardenManager directory

--- GardenManager/src/main/java/com/sharks/GardenManager/DTO/ExampleRequest.java
+++
@@ -1,5 +0,0 @@
-package com.sharks.gardenManager.DTO;
-
-public record ExampleRequest(String[] array, Boolean flag){
-}
-
--- GardenManager/src/main/java/com/sharks/GardenManager/DTO/ExampleResponse.java
+++
@@ -1,3 +0,0 @@
-package com.sharks.gardenManager.DTO;
-
-public record ExampleResponse(String field, String[] array, boolean flag){}
