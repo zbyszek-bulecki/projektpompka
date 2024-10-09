94e424826dd7fe9cafeed4faaff59d658ed7e55a
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2024-10-09 20:34:50 +0200

    more work done on dynamic settings form generation

--- webapp-frontend/src/app/device-settings/device-settings.component.ts
+++ webapp-frontend/src/app/device-settings/device-settings.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { DevicePreviewDataService } from '../services/device-preview-data.service';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RestClientService } from '../services/rest-client.service';
 
@@ -9,21 +14,20 @@ import { RestClientService } from '../services/rest-client.service';
   templateUrl: './device-settings.component.html',
   styleUrls: ['./device-settings.component.css'],
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule]
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class DeviceSettingsComponent {
-
-  constructor (
+  constructor(
     private devicePreviewData: DevicePreviewDataService,
     private restClient: RestClientService,
-    private formBuilder : FormBuilder
-  ) { }
+    private formBuilder: FormBuilder
+  ) {}
 
   macAddress: string | null = null;
   name: string | null = null;
   settingsList: SettingRow[] | null = [];
 
-  settingsForm : FormGroup | null = null;
+  settingsForm: FormGroup | null = null;
 
   ngOnInit(): void {
     this.name = this.devicePreviewData.selected.name;
@@ -31,57 +35,47 @@ export class DeviceSettingsComponent {
     this.loadSettings();
   }
 
-  loadSettings() { 
-    this.restClient.get<SettingRow[]>("/manager/planters/" + this.name + "/" + this.macAddress + "/settings").subscribe(response =>{
-      this.settingsList = response.body;
-      console.log("test");
-      console.log(this.settingsList);
-      this.createForm();
-    });
+  loadSettings() {
+    this.restClient
+      .get<SettingRow[]>(
+        '/manager/planters/' + this.name + '/' + this.macAddress + '/settings'
+      )
+      .subscribe((response) => {
+        this.settingsList = response.body;
+        console.log('test');
+        console.log(this.settingsList);
+        this.createForm();
+      });
   }
-
-  // createFormControl(settingRow : SettingRow) {
-
-  // }
 
   createForm() {
-    if(this.settingsList != null) {
-      const formGroup : Record <string, FormControl> = {};
-      this.settingsList.forEach(s => console.log(s as SettingRow));
-      this.settingsList.forEach(s => formGroup[s.getKey()] = new FormControl(s.getValue()));
+    if (this.settingsList != null) {
+      const formGroup: Record<string, FormControl> = {};
+      this.settingsList.forEach((s) => console.log(s as SettingRow));
+      this.settingsList.forEach((s) => {
+        formGroup[s.key] = new FormControl(s.value);
+        formGroup[s.key + '_default'] = new FormControl(s.isDefault);
+      });
       this.settingsForm = this.formBuilder.group(formGroup);
-      console.log("Cokolwiek");
+      console.log('Cokolwiek');
     }
-    console.log("Coś innego");
+    console.log('Coś innego');
   }
-
 
   submitSettings() {
-    if(this.settingsForm instanceof FormGroup)
-      {console.log(
-        this.settingsForm.value
-    );
-  }
+    if (this.settingsForm instanceof FormGroup) {
+      console.log(this.settingsForm.value);
     }
-
+  }
 }
 
 class SettingRow {
-  constructor (
-    private key: string,
-    private value: string,
-    private isDefault: Boolean
-  ) { }
-
-  getKey(): string{
-    return this.key;
-  }
-
-  getValue(): string{
-    return this.value;
-  }
-
-  getIsDefault(): Boolean{
-    return this.isDefault;
+  public key: string;
+  public value: string;
+  public isDefault: boolean;
+  constructor(key: string, value: string, isDefault: boolean) {
+    this.key = key;
+    this.value = value;
+    this.isDefault = isDefault;
   }
 }
