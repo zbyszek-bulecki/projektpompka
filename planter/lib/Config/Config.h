dacef854db67d8e19edd003fc5218f178ec8acb0
Lemur <nobat@wp.pl>
2025-02-09 10:44:42 +0100

    Added option of saving configs on SD card

--- planter/lib/Config/Config.h
+++ planter/lib/Config/Config.h
@@ -23,23 +23,24 @@ struct keyComparator {
 };
 
 class Config {
-    size_t memorySize=0;
-	char* memory;
 	std::map<char*, char*, keyComparator> configs;
-
-	void loadConfigFile();
-    void parseConfigFile();
+    void parseConfigFile(size_t memorySize, char* tmpMemory);
+	void storeKeyValuePair(char* keyPointer, char* valuePointer);
 	void setValueBasedOnKey(char* key, char* valuePointer);
 public:
 	Config();
 	~Config();
-	void loadConfig();
+	void loadConfigFile();
 	bool has(const char* key);
 	bool has(char* key);
 	char* get(const char* key);
 	char* get(char* key);
+	void set(const char* key, const char* value);
 	int getInt(char* key);
 	int getInt(const char* key);
+	void set(const char* key, int value);
 	float getFloat(char* key);
 	float getFloat(const char* key);
+	void set(const char* key, float value);
+	void saveConfigFile();
 };
